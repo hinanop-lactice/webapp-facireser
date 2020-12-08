b9adfff7356be3789cb44d4bec67709ba3188c10
hina <hina.121230@gmail.com>
2020-12-08 12:05:49 +0900

    マイグレーション作成

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', ['users' => $user]);
+    }
+    public function edit()
+    {
+        $user = Auth::user();
+
+        return view('users.edit', ['user' => $user]);
+    }
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+        $user->fill($request->all());
+        $user->save();
+        
+        return redirect()->back()->with(['message' =>'更新しました！']);
+    }
+
+}
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,11 @@
+@if(Session::has('message'))
+  <div>{{ Session::get('message') }}</div>
+@endif
+
+<form method="POST" action="http://localhost:8000/me">
+  @csrf
+ <label>名前: </label><input name="name"  type="text" value="{{ $user->name }}" />
+ <label>メールアドレス: </label><input name="email"  type="email" value="{{ $user->email }}" />
+ <button type="submit">変更</button>
+</form>
+
